e925385fee360fcc1aeed0e4f8762539e69ca36b
Dawoozies <davidchriswiggins@gmail.com>
2024-10-06 17:17:31 +1100

    bullet impact done

--- Assets/BulletImpact.cs
+++ Assets/BulletImpact.cs
@@ -7,6 +7,7 @@ public class BulletImpact : MonoBehaviour
     public float damage;
     public virtual void Impact()
     {
-
+        ExternalEffects.ins.PlayEffect(1, transform.position);
+        Destroy(gameObject);
     }
 }
